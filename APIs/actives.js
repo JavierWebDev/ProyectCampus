629be017739bc00d71e5d3460ce9e1fdfd4dce76
javierWebDev <151965097+JavierWebDev@users.noreply.github.com>
2024-03-06 08:59:53 -0500

    feat: :sparkles: Started SideBar
    Reorganized DB and  start the development of the navigation menu

--- APIs/actives.js
+++ APIs/actives.js
@@ -1,13 +1,35 @@
 const URL_API = 'http://localhost:3000'
 
-const postData = async (data, endpoint) => {
+const showData = async (data, endpoint, id) => {
     try {
-        return await fetch(`${URL_API}/${endpoint}`, {
+        return await fetch(`${URL_API}/${endpoint}/${id}`, {
+            method: "GET",
+            headers: myHeaders,
+            body: JSON.stringify(data)
+        });
+    } catch (error) {
+        console.error('Error en la solicitud GET:', error.message);
+    }
+}
+const postData = async (data, endpoint, id) => {
+    try {
+        return await fetch(`${URL_API}/${endpoint}/${id}`, {
             method: "POST",
             headers: myHeaders,
             body: JSON.stringify(data)
         });
     } catch (error) {
         console.error('Error en la solicitud POST:', error.message);
+    }
+}
+const updateData = async (newdata, endpoint, id) => {
+    try {
+        return await fetch(`${URL_API}/${endpoint}/${id}`, {
+            method: "PUT",
+            headers: myHeaders,
+            body: JSON.stringify(newdata)
+        });
+    } catch (error) {
+        console.error('Error en la solicitud PUT:', error.message);
     }
 }
--- App/Components/formActivos.js
+++ App/Components/formActivos.js
@@ -1,9 +1,21 @@
 import {postData} from '../../APIs/actives.js'
 
+export class addActives extends HTMLElement {
+    constructor() {
+        super();
+        this.render();
+    }
+    render() {
+        this.innerHTML = /* html */ `
+        <h1></h1>
+        `
+        
+    }
+}
+customElements.define("add-actives",addActives)
 const activo = {
     "nombre" : "juan"
 }
 
 let endpoint = "Actives"
 
-postData(activo, endpoint)
